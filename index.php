94dec2c3dc975d56678a8e2f2efdbcb985b866b5
Mehdi.ZA <mahdi_zarei78@yahoo.com>
2022-11-26 16:21:21 +0330

    NUMBERS FUNCTIONS

--- index.php
+++ index.php
@@ -12,24 +12,33 @@
 // echo htmlentities($link);
 
 
-echo number_format('10000000', 2, '_', '|');
-echo '<br>';
-var_dump(str_split('Hello Mehdi'));
-echo '<br>';
-echo str_replace("Hello", "", "Hello Mehdi");
-echo '<br>';
-echo strcmp('Hello', 'Hellow Mehdi');
-echo '<br>';
-echo strcmp('Hellow Mehdi', 'Hello');
-echo '<br>';
-echo strlen("Mehdi");
-echo '<br>';
-echo strpos('Hi i am mehdi from iran','Mehdi');
-echo '<br>';
-echo strtoupper('Hi i am mehdi from iran');
-echo '<br>';
-echo strtolower('Hi i am mehdi from iran');
-echo '<br>';
-$str='Hello World';
-echo trim($str,'Heo');
+// echo number_format('10000000', 2, '_', '|');
+// echo '<br>';
+// var_dump(str_split('Hello Mehdi'));
+// echo '<br>';
+// echo str_replace("Hello", "", "Hello Mehdi");
+// echo '<br>';
+// echo strcmp('Hello', 'Hellow Mehdi');
+// echo '<br>';
+// echo strcmp('Hellow Mehdi', 'Hello');
+// echo '<br>';
+// echo strlen("Mehdi");
+// echo '<br>';
+// echo strpos('Hi i am mehdi from iran','Mehdi');
+// echo '<br>';
+// echo strtoupper('Hi i am mehdi from iran');
+// echo '<br>';
+// echo strtolower('Hi i am mehdi from iran');
+// echo '<br>';
+// $str='Hello World';
+// echo trim($str,'Heo');
 //  All PHP String Functions ref :https://www.w3schools.com/php/php_ref_string.asp
+
+
+// ******* NUMBERS FUNCTIONS *******//
+$strNum = '120';
+var_dump(is_numeric($strNum));
+echo '<br>';
+$point = 15.8;
+$pointToNum = (int)$point;
+var_dump($pointToNum);
