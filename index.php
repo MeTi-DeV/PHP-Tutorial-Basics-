bdf058b96546818d0156a1968eab29f68ffce685
Mehdi.ZA <mahdi_zarei78@yahoo.com>
2023-01-21 23:31:11 +0330

    complete functions section

--- index.php
+++ index.php
@@ -125,40 +125,66 @@
 // ******* Loops part *******//
 
 
-$array = ['Benz', 'BMW', 'Volvo'];
-for ($counter = 0; $counter < count($array); $counter++) {
-    echo $array[$counter] . ' ';
-}
-$acArray = [
-    ['a' => 'Mehdi', 'code' => '1234'],
-    ['b' => 'Yashar', 'code' => '7689']
-];
+// $array = ['Benz', 'BMW', 'Volvo'];
+// for ($counter = 0; $counter < count($array); $counter++) {
+//     echo $array[$counter] . ' ';
+// }
+// $acArray = [
+//     ['a' => 'Mehdi', 'code' => '1234'],
+//     ['b' => 'Yashar', 'code' => '7689']
+// ];
+// 
+// 
+// for ($i = 0; $i < count($acArray); $i++) {
+//     $acArray[$i]['rand'] = rand(1111, 9999);
+// }
+// 
+// var_dump($acArray);
+// $colors = ['Green', 'Red', 'Blue', 'Yellow'];
+// foreach ($colors as $color) {
+//     echo $color . ' ';
+// }
+// 
+// $users = [['Mehdi', 'Negin'], ['Reza', 'Mahshid'],];
+// foreach ($users as $user) {
+//     foreach ($user as $name) {
+//         echo $name . ' ';
+//     }
+// }
+// $data = ['name' => 'Mehdi', 'age' => 23, 'job' => 'Programmer'];
+// foreach ($data as $key => $value) {
+//     echo $key . ' => ' . $value . ' ';
+// }
+// $counter=0;
+// for ($counter; $counter <20 ; $counter++) { 
+//     if (!($counter%2==0)) {
+//         continue;
+//     }
+//     echo $counter.' , ';
+// }
+// ******* Fuunction part *******//
 
 
-for ($i = 0; $i < count($acArray); $i++) {
-    $acArray[$i]['rand'] = rand(1111, 9999);
-}
-
-var_dump($acArray);
-$colors = ['Green', 'Red', 'Blue', 'Yellow'];
-foreach ($colors as $color) {
-    echo $color . ' ';
-}
-
-$users = [['Mehdi', 'Negin'], ['Reza', 'Mahshid'],];
-foreach ($users as $user) {
-    foreach ($user as $name) {
-        echo $name . ' ';
-    }
-}
-$data = ['name' => 'Mehdi', 'age' => 23, 'job' => 'Programmer'];
-foreach ($data as $key => $value) {
-    echo $key . ' => ' . $value . ' ';
-}
-$counter=0;
-for ($counter; $counter <20 ; $counter++) { 
-    if (!($counter%2==0)) {
-        continue;
-    }
-    echo $counter.' , ';
-}
+// function showNumbers($num)
+// {
+//     if ($num <= 5) {
+//         echo $num . ' ';
+//         showNumbers($num + 1);
+//         return $num;
+//     }
+// }
+// showNumbers(1);
+// function my_callback($item)
+// {
+//     return strlen($item);
+// }
+// 
+// $result = array_map('my_callback', ['Green', 'Red', 'Blue']);
+// var_dump($result);
+// function adder($item)
+// {
+//     return $item + 2;
+// }
+// 
+// $result = array_map('adder', [3, 5, 7]);
+// var_dump($result);
