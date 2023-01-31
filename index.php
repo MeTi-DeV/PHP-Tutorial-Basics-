7280ae77aa6e04c5dd0168ea525b06208484cb61
MeTi-DeV <mahdi_zarei78@yahoo.com>
2023-01-31 21:57:16 +0330

    work with files in php

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <title>Form</title>
     <div style="padding: 32px;">
-        <form action="main.php" ,method="GET">
+        <form action="main.php" ,method="POST" enctype="multipart/form-data">
             <div class="mb-3 row">
                 <label for="" class="col-sm-2 col-form-label">First Name</label>
                 <div class="col-sm-10">
@@ -21,19 +21,10 @@
                     <input type="email" class="form-control" name="email">
                 </div>
             </div>
-            <button class="btn btn-primary" type="submit">Submit</button>
-        </form>
-        <form action="main.php" ,method="POST">
             <div class="mb-3 row">
-                <label for="" class="col-sm-2 col-form-label">First Name</label>
-                <div class="col-sm-10">
-                    <input type="text" class="form-control" name="first_name">
-                </div>
-            </div>
-            <div class="mb-3 row">
-                <label for="" class="col-sm-2 col-form-label">Email</label>
-                <div class="col-sm-10">
-                    <input type="email" class="form-control" name="email">
+                <label for="" class="col-sm-2 col-form-label">Image</label>
+                <div class="col-sm-3">
+                    <input type="file" class="form-control" name="email">
                 </div>
             </div>
             <button class="btn btn-primary" type="submit">Submit</button>
