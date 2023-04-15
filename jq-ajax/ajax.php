50a16d03d5160fdc0eebb541f9ffd97cb1127878
Mehdi.ZA <mahdi_zarei78@yahoo.com>
2023-04-15 22:46:36 +0330

    jq-ajax form submit

--- jq-ajax/ajax.php
+++ jq-ajax/ajax.php
@@ -1,3 +1,8 @@
-<?php 
-echo 'hi'
+<?php
+if (isset($_POST['username']) && $_POST['username'] == 'Mehdi' && isset($_POST['password']) && $_POST['password'] == '1234') {
+ echo   json_encode(['success' => 1]);
+} else {
+  echo  json_encode(['success' => 0]);
+
+}
 ?>
--- jq-ajax/script.js
+++ jq-ajax/script.js
@@ -1,15 +1,26 @@
 $(document).ready(function () {
-    $('#myBtn').click(function () {
-        $.ajax('ajax.php', {
 
-            success: function (data) {
-                alert('from server' + ' ' + data)
-            },
-            error: function () {
-                alert('Somethings Wrong ')
-            }
-        });
-    })
+    $('#myform').submit(function (e) {
+        e.preventDefault();
+
+                $.ajax({
+                    type: "POST",
+                    url: "ajax.php",
+                    data: $(this).serialize(),
+        
+                    success: function (response) {
+                        var jsonData = JSON.parse(response);
+                        if (jsonData.success == 1) {
+                            location.href = 'panel.php';
+                        } else {
+                            alert('اطلاعات وارد شده اشتباه می باشد');
+                        }
+                    }
+                });
+
+    });
+
+
 
 
 });
